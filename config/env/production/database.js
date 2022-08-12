be90a165e6d84735b095dc2a5b67d7756325d700
habibeshadow <habibe@kayfo.games>
2022-08-12 08:05:39 +0000

    update db conf

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'ec2-52-207-15-147.compute-1.amazonaws.com'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'd5360hs5la7ui'),
-      user: env('DATABASE_USERNAME', 'fbjhxifjltliyc'),
-      password: env('DATABASE_PASSWORD', '5542fd179be40b270d337a5d4c9e197847c0927d14ea8206601bdab29e3a00f4'),
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
       // schema: env('DATABASE_SCHEMA', 'public'), // Not required
       ssl: {
         rejectUnauthorized: false
