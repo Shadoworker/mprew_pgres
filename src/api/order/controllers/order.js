14f17e065531532f5785e2a5d47c64579e4d1616
habibeshadow <habibe@kayfo.games>
2022-10-27 15:05:45 +0000

    Vid tuto link + New mesurements props

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -8,15 +8,4 @@ const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::order.order');
 
-// module.exports = createCoreController("api::order.order", ({ strapi }) => ({
-//     create(ctx) {
-//       ctx.request.body = {
-//         data: {
-//           ...ctx.request.body.data,
-//           user: ctx.state.user.id,
-//         },
-//       };
-  
-//       return super.create(ctx);
-//     },
-//   }));
+ 
